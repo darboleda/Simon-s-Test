acd647a9fa173f401cc5d25d2ee33e743aed421d
Don Arboleda <darboleda@zynga.com>
2014-12-18 15:22:29 -0800

    Preliminary bgm manager

---
+++ Assets/Canal/Games/BgmManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Canal.Unity;
+
+[RequireComponent(typeof(AudioSource))]
+public class BgmManager : Behavior
+{
+    public void SetBgm(AudioClip audioClip)
+    {
+        AudioSource source = this.GetComponent<AudioSource>();
+        if (source.clip != audioClip)
+        {
+            source.Stop();
+            source.clip = audioClip;
+            source.Play();
+        }
+    }
+}
